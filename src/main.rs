5a59736e27aa543fdaeaf72eac1fcf98c8cb53b2
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-08-10 13:12:59 +0100

    Add comments on how to run the CLI

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use sc::{Config, run};
 use clap::Parser;
 
 fn main() {
+    // sc/src cargo run -- --word <word>
+    // sc/src cargo run -- <file_path>
+
     let config = Config::parse();
 
     if let Err(error) = run(&config) {
